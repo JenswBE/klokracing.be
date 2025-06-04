8dc5af780a67b5b3df695c6ddecab51fb244ee8e
JenswBE <6514515+JenswBE@users.noreply.github.com>
2025-06-04 20:22:50 +0200

    Update "openingsuren" in footer

--- data/openingsuren.go
+++ data/openingsuren.go
@@ -12,7 +12,7 @@ type OpeningsurenContact struct {
 
 func GetOpeningsuren() Openingsuren {
 	return Openingsuren{
-		Footer: "MA, DI, DO: 18h - 20h30 • WOE: 18h - 20h30 • VR: 9h - 18h",
+		Footer: "MA, DI, DO: 18h - 20h30 • VR: 9h - 17h30",
 		Contact: []OpeningsurenContact{
 			{
 				Name: "MAANDAG",
